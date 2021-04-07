c6ec6caca78c72b8a0529ef1d4b9c7cb45b17499
VNKL <vnkl.iam@gmail.com>
2021-04-07 11:46:51 +0300

    change entrypoint.sh

--- docker/api/entrypoint.sh
+++ docker/api/entrypoint.sh
@@ -6,6 +6,6 @@ python manage.py makemigrations --noinput
 python manage.py migrate --noinput
 python manage.py init_admin
 python manage.py init_accounts
-python manage.py continue_tasks
+python manage.py continue_tasks &
 
-exec gunicorn api.wsgi:application -b 0.0.0.0:8000 --reload
+exec gunicorn api.wsgi:application -b 0.0.0.0:8000 --reload --log-level debug
