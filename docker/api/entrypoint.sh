5b2143dd8f7749828b6ae3b4809077a370cf69fe
VNKL <vnkl.iam@gmail.com>
2021-04-30 19:17:42 +0300

    fixes

--- docker/api/entrypoint.sh
+++ docker/api/entrypoint.sh
@@ -8,4 +8,4 @@ python manage.py init_admin
 python manage.py init_accounts
 python manage.py continue_tasks &
 
-exec gunicorn api.wsgi:application -b 0.0.0.0:8000 --reload --log-level debug
+exec gunicorn api.wsgi:application -b 0.0.0.0:8000 --reload --log-level debug --limit-request-line 8190 --timeout 1000
